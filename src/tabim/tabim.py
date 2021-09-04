62565b5c80eaded6c8a6c503106df66e034126e2
Tamir Bahar <1386239+tmr232@users.noreply.github.com>
2021-09-04 17:23:38 +0300

    With control!
    Still a total mess...

---
+++ src/tabim/tabim.py
@@ -0,0 +1,140 @@
+import functools
+import operator
+from operator import attrgetter
+from typing import Optional, NewType, List
+
+import attr
+import guitarpro.models
+
+
+@attr.frozen
+class Note:
+    start: int
+    end: int
+    string: int
+    fret: int
+
+
+@attr.define
+class Measure:
+    notes: List[Note]
+
+    @property
+    def columns(self):
+        # This is terrible code, but we need to start somewhere.
+        start_times = sorted({note.start for note in self.notes})
+
+        for start in start_times:
+            # Collect all the notes that start or continue at given time
+            starting = [note for note in self.notes if note.start == start]
+            cont = [note for note in self.notes if note.start < start < note.end]
+
+            # Convert to display notes
+            # We start with the continuation notes to handle silly mistakes
+            strings = {n: DisplayNote() for n in range(1, 7)}
+            for note in cont:
+                strings[note.string] = DisplayNote(
+                    cont_in=True,
+                    cont=True,
+                    fret=note.fret,
+                )
+            for note in starting:
+                strings[note.string] = attr.evolve(
+                    strings[note.string],
+                    fret=note.fret,
+                    cont=False,
+                    cont_out=True,
+                )
+
+            yield [note for _, note in sorted(strings.items())]
+
+
+@attr.define
+class DisplayNote:
+    cont_in: bool = False
+    cont_out: bool = False
+    cont: bool = False
+    fret: Optional[int] = None
+
+
+@attr.define
+class RenderedNote:
+    prefix: str
+    postfix: str
+    pre_cont: str
+    post_cont: str
+
+    def __str__(self):
+        return f"{self.pre_cont}_{self.prefix}.{self.postfix}_{self.post_cont}"
+
+
+Column = NewType("Column", List[DisplayNote])
+
+
+def parse_measure(measure: guitarpro.models.Measure) -> Measure:
+    notes = []
+
+    for voice in measure.voices:
+        for beat in voice.beats:
+            for note in beat.notes:
+                notes.append(
+                    Note(
+                        start=beat.startInMeasure,
+                        end=beat.startInMeasure + beat.duration.time,
+                        fret=note.value,
+                        string=note.string,
+                    )
+                )
+    return Measure(notes=sorted(notes, key=attrgetter("start")))
+
+
+def render_note(note: DisplayNote) -> RenderedNote:
+    if note.cont_in:
+        pre_cont = "="
+    else:
+        pre_cont = "-"
+    if note.cont_out:
+        post_cont = "="
+    else:
+        post_cont = "-"
+    prefix = ""
+    if note.cont:
+        postfix = "="
+    elif note.fret is None:
+        postfix = "-"
+    else:
+        postfix = str(note.fret)
+
+    return RenderedNote(
+        prefix=prefix,
+        postfix=postfix,
+        pre_cont=pre_cont,
+        post_cont=post_cont,
+    )
+
+
+def concat_columns(col1: str, col2: str) -> str:
+    return "\n".join(map(operator.add, col1.splitlines(), col2.splitlines()))
+
+
+def render_column(column: Column, min_width: int) -> str:
+    rendered: List[RenderedNote] = list(map(render_note, column))
+    max_prefix = max(len(n.prefix) for n in rendered)
+    max_postfix = max(len(n.postfix) for n in rendered)
+
+    max_prefix = max(min_width // 2, max_prefix)
+    max_postfix = max(min_width // 2 + min_width % 2, max_postfix)
+
+    return "\n".join(
+        note.prefix.rjust(max_prefix, note.pre_cont)
+        + note.postfix.ljust(max_postfix, note.post_cont)
+        for note in rendered
+    )
+
+
+def render_measure(measure: Measure, column_width: int) -> str:
+    return functools.reduce(
+        concat_columns,
+        map(lambda col: render_column(col, column_width), measure.columns),
+        "\n".join("|" * 6),
+    )
--- tests/test_render.py
+++ tests/test_render.py
@@ -1,109 +1,48 @@
-from typing import Optional, NewType, List
+import guitarpro
 
-import attr
-
-
-@attr.define
-class Note:
-    cont_in: bool
-    cont_out: bool
-    cont: bool
-    fret: Optional[int]
-
-
-@attr.define
-class RenderedNote:
-    prefix: str
-    postfix: str
-    pre_cont: str
-    post_cont: str
-
-    def __str__(self):
-        return f"{self.pre_cont}_{self.prefix}.{self.postfix}_{self.post_cont}"
-
-
-Column = NewType("Column", List[Note])
-
-
-def render_note(note: Note) -> RenderedNote:
-    if note.cont_in:
-        pre_cont = "="
-    else:
-        pre_cont = "-"
-    if note.cont_out:
-        post_cont = "="
-    else:
-        post_cont = "-"
-    prefix = ""
-    if note.cont:
-        postfix = "="
-    elif note.fret is None:
-        postfix = "-"
-    else:
-        postfix = str(note.fret)
-
-    return RenderedNote(
-        prefix=prefix,
-        postfix=postfix,
-        pre_cont=pre_cont,
-        post_cont=post_cont,
-    )
-
-
-def render_column(column: Column, min_width: int) -> str:
-    rendered: List[RenderedNote] = list(map(render_note, column))
-    max_prefix = max(len(n.prefix) for n in rendered)
-    max_postfix = max(len(n.postfix) for n in rendered)
-
-    max_prefix = max(min_width // 2, max_prefix)
-    max_postfix = max(min_width // 2 + min_width % 2, max_postfix)
-
-    return "\n".join(
-        note.prefix.rjust(max_prefix, note.pre_cont)
-        + note.postfix.ljust(max_postfix, note.post_cont)
-        for note in rendered
-    )
+from tabim.tabim import DisplayNote, render_column, parse_measure, render_measure
+from tests.conftest import get_sample
 
 
 def test_render_column():
     column = [
-        Note(
+        DisplayNote(
             cont_in=False,
             cont_out=False,
             cont=False,
             fret=None,
         ),
-        Note(
+        DisplayNote(
             cont_in=False,
             cont_out=False,
             cont=False,
             fret=6,
         ),
-        Note(
+        DisplayNote(
             cont_in=False,
             cont_out=True,
             cont=False,
             fret=6,
         ),
-        Note(
+        DisplayNote(
             cont_in=True,
             cont_out=False,
             cont=False,
             fret=6,
         ),
-        Note(
+        DisplayNote(
             cont_in=True,
             cont_out=True,
             cont=False,
             fret=6,
         ),
-        Note(
+        DisplayNote(
             cont_in=True,
             cont_out=True,
             cont=True,
             fret=None,
         ),
-        Note(
+        DisplayNote(
             cont_in=True,
             cont_out=False,
             cont=True,
@@ -111,4 +50,16 @@ def test_render_column():
         ),
     ]
     print()
-    print(render_column(column, 3))
+    print(render_column(column, 8))
+
+
+def test_from_gp():
+    with open(get_sample("BasicSustain.gp5"), "rb") as stream:
+        tab = guitarpro.parse(stream)
+
+    track = tab.tracks[0]
+
+    for gp_measure in track.measures:
+        measure = parse_measure(gp_measure)
+        print()
+        print(render_measure(measure, 8))
