8fdfe7bf6bc6a2d7f08071d7ddf18c20e0b36fe5
Tamir Bahar <1386239+tmr232@users.noreply.github.com>
2021-09-11 16:17:42 +0300

    Parse song!

--- tests/test_beats.py
+++ tests/test_beats.py
@@ -1,10 +1,15 @@
+from __future__ import annotations
 import re
 from collections import deque
 from itertools import groupby
 from operator import attrgetter
-from typing import Iterable, Any, Optional
+from typing import Iterable, Any, Optional, Iterator
 
+import attr
 import guitarpro
+import rich
+
+from tests.conftest import get_sample
 
 
 def unnest(iterable: Iterable[Any], *attrs: str, extra: Optional[str] = None):
@@ -26,21 +31,55 @@ def unnest(iterable: Iterable[Any], *attrs: str, extra: Optional[str] = None):
         yield from items
 
 
+@attr.s(auto_attribs=True)
+class TabNote:
+    note: guitarpro.Note
+    is_play: bool = False
+    is_cont: bool = False
+    is_tie: bool = False
+
+    @staticmethod
+    def play(note: guitarpro.Note) -> TabNote:
+        return TabNote(note=note, is_play=True)
+
+    @staticmethod
+    def cont(note: guitarpro.Note) -> TabNote:
+        return TabNote(note=note, is_cont=True)
+
+
+@attr.s(auto_attribs=True)
 class TabBeat:
+    start: int
+    notes: list[TabNote] = attr.Factory(list)
     is_measure_break: bool = False
     is_rest: bool = False
+    lyric: Optional[str] = None
+
+    @staticmethod
+    def from_notes(
+        start: int, notes: list[TabNote], lyric: Optional[str] = None
+    ) -> TabBeat:
+        return TabBeat(notes=notes, lyric=lyric, start=start)
+
+    @staticmethod
+    def measure(start: int) -> TabBeat:
+        return TabBeat(is_measure_break=True, start=start)
 
 
-def get_measure_beats(measure: guitarpro.Measure):
+def get_measure_beats(measure: guitarpro.Measure) -> list[guitarpro.Beat]:
     beats = unnest(measure.voices, "beats")
     return sorted(beats, key=attrgetter("start"))
 
 
-def get_grouped_beats(measure: guitarpro.Measure):
-    return groupby(get_measure_beats(measure), key=attrgetter("start"))
+def get_grouped_beats(
+    measure: guitarpro.Measure,
+) -> Iterator[tuple[int, Iterator[guitarpro.Beat]]]:
+    return groupby(get_measure_beats(measure), key=attrgetter("start"))  # type:ignore
 
 
-def parse_lyrics(lyric_line: guitarpro.LyricLine, track: guitarpro.Track):
+def parse_lyrics(
+    lyric_line: guitarpro.LyricLine, track: guitarpro.Track
+) -> dict[int, str]:
     timestamps = []
     for measure in track.measures[lyric_line.startingMeasure - 1 :]:
         for beat in measure.voices[0].beats:
@@ -77,5 +116,51 @@ def parse_song(song: guitarpro.Song, track_number: int = 0):
     """
     track = song.tracks[track_number]
 
+    lyric_timestamps = parse_lyrics(song.lyrics.lines[0], track)
+
+    tab_beats = []
+    live_notes: list[Optional[guitarpro.Note]] = [None for _ in track.strings]
     for measure in track.measures:
-        beats = get_measure_beats(measure)
+        for timestamp, beats in get_grouped_beats(measure):
+            # Remove all ended live-notes
+            for string, note in enumerate(live_notes):
+                if not note:
+                    continue
+                if note.beat.start + note.beat.duration.time < timestamp:
+                    live_notes[string] = None
+
+            # Collect new notes from current beats
+            notes: list[Optional[TabNote]] = [None for _ in track.strings]
+            new_live_notes = live_notes[:]
+            for beat in beats:
+                for note in beat.notes:
+                    new_live_notes[note.string - 1] = note
+                    notes[note.string - 1] = TabNote.play(note)
+
+            # Mark continuation for live notes
+            for string, (note, live_note) in enumerate(zip(notes, live_notes)):
+                if note:
+                    continue
+                if live_note:
+                    notes[string] = TabNote.cont(live_note)
+
+            # Update live notes
+            live_notes = new_live_notes
+
+            tab_beats.append(
+                TabBeat.from_notes(
+                    notes=notes, lyric=lyric_timestamps.get(timestamp), start=timestamp
+                )
+            )
+
+        tab_beats.append(TabBeat.measure(start=measure.end))
+
+    return tab_beats
+
+
+def test_parse_song():
+    with get_sample("BasicSustain.gp5").open("rb") as stream:
+        song = guitarpro.parse(stream)
+
+    tab = parse_song(song, 0)
+    rich.print(tab)
