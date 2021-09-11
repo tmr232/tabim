6bbeb2bdae140f0961f1b56ea319d9ab60a94c0f
Tamir Bahar <1386239+tmr232@users.noreply.github.com>
2021-09-11 10:33:41 +0300

    Docs

--- tests/test_beats.py
+++ tests/test_beats.py
@@ -54,6 +54,27 @@ def parse_lyrics(lyric_line: guitarpro.LyricLine, track: guitarpro.Track):
 
 
 def parse_song(song: guitarpro.Song, track_number: int = 0):
+    """
+    The idea is that we break the song up into yet another new kind of beat.
+    This itme, the beat holds notes of the following types:
+
+        * ``Play(note)`` - this is where the note starts
+        * ``Cont(note)`` - The note is still active
+        * ``Tie(note, into)`` - Ties to the previous note. Also holds the original note.
+
+    So that later we can iterate and know what to render.
+    Additionally, a beat can also have a "measure-bound" type so that
+    we just know to draw it.
+
+    The idea is that as we traverse the notes, we build the beats to
+    hold all the relevant information.
+    Later, when we actually draw - we traverse in pairs (or triplets?) to
+    know how to apply effects and continuations.
+    But the basic knowledge of whether to apply continuations is already present.
+
+    Beats will also hold info on whether they are "Strong" beats (quarters in 4/4, for example)
+    and the relevant lyric fragment.
+    """
     track = song.tracks[track_number]
 
     for measure in track.measures:
